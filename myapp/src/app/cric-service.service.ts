adbcee3f8e32884bc9cff05ac50735f6669db772
Ahsan Sohail <k142109@nu.edu.pk>
2017-08-23 22:41:35 +0500

    Successfull API CALL

--- myapp/src/app/cric-service.service.ts
+++ myapp/src/app/cric-service.service.ts
@@ -1,19 +1,26 @@
 import { Injectable } from '@angular/core';
 import {Http} from '@angular/http';
+import {parseString} from 'xml2js'
 import 'rxjs'
 
 @Injectable()
 export class CricServiceService {
 
-  baseUrl : String;
+  baseUrl : string;
+  parser;
 
   constructor(private http : Http) {
-    this.baseUrl = "http://cricscore-api.appspot.com/";
+    this.baseUrl = "http://localhost:1337/synd.cricbuzz.com/j2me/1.0/livematches.xml";
   }
 
   getAllMatch(){
-    return this.http.get(this.baseUrl + 'csa')
-            .map(res=>res.json);
-  }
+    return this.http.get(this.baseUrl)
+            .map(res=>{
+              parseString(res, function (err, result) {
+                console.dir(JSON.stringify(result));
+            });
+              
+        })
+    }
 
 }
